0313207b17f456090cb0eab09066531b53583b8a
Shm1ks <maksimka9331234@gmail.com>
2023-08-23 10:45:48 +0300

    test10

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,11 +7,11 @@ from dotenv import load_dotenv
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = ('django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$')
+SECRET_KEY = os.getenv('TOKEN')
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', default='127.0.0.1,localhost').split(', ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', default='127.0.0.1, localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -102,7 +102,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
