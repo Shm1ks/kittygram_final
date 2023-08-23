07b429ada17bde13cb3463d66ec736489fb0e74e
Shm1ks <maksimka9331234@gmail.com>
2023-08-23 10:52:41 +0300

    test11

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('TOKEN')
+SECRET_KEY = os.getenv('TOKEN', default=' ')
 
 DEBUG = True
 
